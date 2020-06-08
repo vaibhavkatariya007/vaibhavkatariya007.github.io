d5763ee78b3b98a99cefc1bf0351498ff7014301
vaibhavAccionLabs <vaibhav.katariya@accionlabs.com>
2020-06-08 10:04:29 +0530

    My Resume

---
+++ precache-manifest.f4cb706f15ed9452c8c560448167c616.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b0fdd8cc9164f34865",
+    "url": "/css/about.dda57518.css"
+  },
+  {
+    "revision": "64fede1ba119257ab29c",
+    "url": "/css/app.c89534f5.css"
+  },
+  {
+    "revision": "7fdf4aa16efd2717db0b",
+    "url": "/css/chunk-vendors.6326393c.css"
+  },
+  {
+    "revision": "ee7aa5211ae6c8e7eaab",
+    "url": "/css/contact.a6b15ada.css"
+  },
+  {
+    "revision": "278d9283d50d85909c6c856b66d3980d",
+    "url": "/img/bg.278d9283.png"
+  },
+  {
+    "revision": "3e49ca701344805b064143e05e12466d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64b0fdd8cc9164f34865",
+    "url": "/js/about.9214db11.js"
+  },
+  {
+    "revision": "64fede1ba119257ab29c",
+    "url": "/js/app.2ba085b0.js"
+  },
+  {
+    "revision": "5dc1b3d6c63e7dd924f7",
+    "url": "/js/blog.8379ed45.js"
+  },
+  {
+    "revision": "7fdf4aa16efd2717db0b",
+    "url": "/js/chunk-vendors.7453b5f7.js"
+  },
+  {
+    "revision": "ee7aa5211ae6c8e7eaab",
+    "url": "/js/contact.a3378734.js"
+  },
+  {
+    "revision": "4e06063fa97395318ed5",
+    "url": "/js/portfolio.d17dd32c.js"
+  },
+  {
+    "revision": "e5f2a2e26ae7c850c67449988f9e250e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
